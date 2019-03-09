73c47136cc2774d7189336f8f09883425120d4f5
Dmytro Haidaichuk <dmytro.gaidaichuk@gmail.com>
2019-03-09 12:55:12 +0100

    Make number and check if you guessed

---
+++ src/guess/GuessTheNumber.java
@@ -0,0 +1,30 @@
+package guess;
+
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    public static void main(String[] args) {
+        int numUser;
+        int numGuess = randomNumber(1, 100);
+        System.out.println(":) cheat  random number --> " + numGuess);
+        while ((numUser = getUserNumber()) != numGuess) {
+            System.out.print(numUser < numGuess ? "Za mało! " : "Za duzo! ");
+        }
+        System.out.println("Zgadłeś");
+    }
+
+    public static int getUserNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Zgadnij liczbę: ");
+        while (!sc.hasNextInt()) {
+            System.out.print("To nie jest liczba. Zgadnij liczbę: ");
+            sc.nextLine();
+        }
+        return sc.nextInt();
+    }
+
+    public static int randomNumber(int min, int max) {
+        return (int) (min + Math.random() * (max - min));
+    }
+}
