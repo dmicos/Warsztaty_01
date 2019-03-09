cc7856c87d3f67fe34a8d6ec62aa20ddd94512be
Dmytro Haidaichuk <dmytro.gaidaichuk@gmail.com>
2019-03-09 14:18:46 +0100

    Number guessing game v2

--- src/guess2/GuessTheNumber2.java
+++ src/guess2/GuessTheNumber2.java
@@ -1,4 +1,56 @@
 package guess2;
 
+import java.util.Scanner;
+
 public class GuessTheNumber2 {
+
+    public static void main(String[] args) {
+        int min = 0;
+        int max = 1001;
+        int count = 0;
+
+        while (count <= 10) {
+            count++;
+            int guess = getGuess(min, max);
+            System.out.println("Czy ta liczba to: " + guess + "?");
+            int answer = getAnswer();
+            if (answer == 1) {
+                min = guess;
+            } else if (answer == 2) {
+                max = guess;
+            } else if (answer == 3) {
+                System.out.println("Trafiłem w " + count + " ruchach");
+                break;
+            } else {
+                System.out.println("--- Nie oszukuj! ---");
+                count--;
+            }
+            System.out.println("-----------------");
+        }
+        if (count > 10) {
+            System.out.println("Jesteś oszustem");
+        }
+    }
+
+    public static int getGuess(int min, int max) {
+        return (int) (min + (max - min) / 2);
+    }
+
+    public static int getAnswer() {
+        System.out.print(
+                "[1] więcej\n" +
+                        "[2] mniej\n" +
+                        "[3] trafiłeś\n" +
+                        "Pobierz odpowiedz [1-3]: "
+        );
+
+        Scanner sc = new Scanner(System.in);
+        while (!sc.hasNextInt()) {
+            System.out.println("Pobierz odpowiedz [1-3]: ");
+            sc.nextLine();
+        }
+
+        return sc.nextInt();
+    }
+
 }
