a93ffafbdd258b5a681eea735b21df298b1debec
Dmytro Haidaichuk <dmytro.gaidaichuk@gmail.com>
2019-03-12 01:20:43 +0100

    Added closure BuffereWriter

--- src/site/PopularWords.java
+++ src/site/PopularWords.java
@@ -13,10 +13,11 @@ public class PopularWords {
 
     public static void main(String[] args) {
         Connection connect = Jsoup.connect("http://www.onet.pl/");
-        try {
+        try (
+                BufferedWriter writer = new BufferedWriter(new FileWriter("popular_words.txt"))
+        ) {
             Document document = connect.get();
             Elements links = document.select("span.title");
-            BufferedWriter writer = new BufferedWriter(new FileWriter("popular_words.txt"));
             for (Element elem : links) {
                 for (String word : elem.text().replaceAll("\\p{Punct}", " ").toLowerCase().split("\\s+")) {
                     if (word.length() > 3) {
@@ -34,9 +35,10 @@ public class PopularWords {
     }
 
     public static void filteredWords() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("popular_words.txt"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter("filtered_popular_words.txt"));
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("popular_words.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("filtered_popular_words.txt"))
+        ) {
             String line;
             while ((line = reader.readLine()) != null) {
                 if (!checkFilter(line)) {
